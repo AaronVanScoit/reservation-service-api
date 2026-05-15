2a469f8920172cb6384f3beff6625f088a213b05
ItsVanScoit <ianvanscoit02@gmail.com>
2026-05-14 23:21:51 -0600

    Se hizo el end point de Search Available Vehicles

--- src/main/java/org/binarybrains/SearchAvailableVehicles.java
+++ src/main/java/org/binarybrains/SearchAvailableVehicles.java
@@ -2,14 +2,11 @@ package org.binarybrains;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 
 @Path("/AvailableVehicles")
 public class SearchAvailableVehicles {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String sav() {
         return "Chinga a tu madre pelon";
     }
